721fcfdb4bcac303f187137d62eadb52da3583cc
Gautam <gautam1237@gmail.com>
2017-01-27 06:17:55 +0900

    Added the basic code for Rooms

--- ircd.py
+++ ircd.py
@@ -22,12 +22,13 @@ def parsemsg(s):
 	command = args.pop(0)
 	return prefix, command, args
 
-class ChatServer(IRC):
+class User(IRC):
 	OUTPUT_DENOTER = "<= "
 	INPUT_DENOTER = "=> "
 
-	def __init__(self, users):
+	def __init__(self, users, rooms):
 		self.users = users
+		self.rooms = rooms
 		self.name = None
 		self.state = "GETNAME"
 
@@ -44,7 +45,8 @@ class ChatServer(IRC):
 
 	def dataReceived(self,data):
 		if data[0] == '/':
-			self.handle_Command(data) 
+			self.handle_Command(data)
+			return 
 
 		if self.state == "GETNAME":
 			self.handle_GETNAME(data.rstrip())
@@ -72,31 +74,79 @@ class ChatServer(IRC):
 
 	def handle_GETNAME(self, name):
 		if name in self.users:
-			self.sendToClient("Sorry, name taken")
+			self.sendToClient("Sorry, name taken.")
 			return
-		self.sendToClient("Welcome, {}!".format(name))
+		self.sendToClient("Welcome {}!".format(name))
 		self.name = name
-		self.users[name] = self
+		self.users[name] = [self,None]
 		self.state = "CHAT"
 
 	def handle_CHAT(self, message):
 		message = "<%s> %s" % (self.name, message)
 		for name, protocol in self.users.iteritems():
-			if protocol != self:
-				protocol.sendLine(message)
+			if protocol[0] != self:
+				protocol[0].sendLine(message)
 
 	def irc_unknown(self, prefix, command, params):
-		print "%s, %s, %s, IRC UNKNOWN" % (prefix, command, params)
+		self.sendToClient("{}, {}, {}, IRC UNKNOWN".format(prefix, command, params))
+
+	def irc_ROOMS(self, prefix, params):
+		self.sendToClient("Active rooms are:")
+		for room_name,room in self.rooms.iteritems():
+			self.sendToClient("*" + room_name + " (" + str(len(room.users)) + ")")
+		self.sendToClient("end of list.")
+
+	def irc_JOIN(self, prefix, params):
+		if not params:
+			self.sendToClient("Please use : /join <room-name>")
+		else:
+			self.sendToClient("entering room: {}".format(params[0]))
+			self.joinRoom(params[0])
+			listUsers(params[0])
+
+	def listUsers(self,room_name):
+		for user in self.rooms[room_name].users:
+			if user == self.name:
+				self.sendToClient("* " + user + "(** this is you)")
+			else:
+				self.sendToClient("* " + user)
+
+	def joinRoom(self, room_name):
+		if room_name in self.rooms:
+			room = self.rooms[room_name]
+		else:
+			room = Room(room_name)
+			self.rooms[room_name] = room
+		self.users[self.name][1] = room
+		room.addUser(self.name)
 
 
-class ChatServerFactory(Factory):
+class ChatServer(Factory):
 
 	def __init__(self):
 		self.users = {} # maps user names to Chat instances
+		self.rooms = {} # mapr rooms to Chat isntances
 
 	def buildProtocol(self, addr):
-		return ChatServer(self.users)
+		return User(self.users, self.rooms)
 
+class Room:
 
-reactor.listenTCP(55555, ChatServerFactory())
+	def __init__(self, name):
+		self.name = name
+		self.users = []
+
+	def addUser(self, user):
+		if user in self.users:
+			print "User already in room {}".format(self.name)
+		else:
+			self.users = self.users + [user]
+
+	def delUser(self, user):
+		if user in self.users:
+			self.users.remove(user)
+		else:
+			print "User not in room {}".format(self.name)
+
+reactor.listenTCP(55555, ChatServer())
 reactor.run()
